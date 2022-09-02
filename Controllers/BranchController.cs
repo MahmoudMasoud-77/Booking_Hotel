c385fd483fb68a8ac44a12e583849c576140cba9
M_Masoud <mahmoudsaied7760@gmail.com>
2022-09-02 04:48:00 +0200

    Adding Room and Branch Controller

---
+++ Controllers/BranchController.cs
@@ -0,0 +1,32 @@
+﻿using Booking_Hotel.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking_Hotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchController : ControllerBase
+    {
+        private readonly IBranchService Service;
+
+        public BranchController(IBranchService _Service)
+        {
+            this.Service = _Service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var data = await Service.GetAllAsync();
+                
+                    return Ok(data);
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/RoomController.cs
@@ -0,0 +1,34 @@
+﻿using Booking_Hotel.Data.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Booking_Hotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService Service;
+
+        public RoomController(IRoomService _Service)
+        {
+            this.Service = _Service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var data = await Service.GetAllAsync();
+
+                return Ok(data);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
