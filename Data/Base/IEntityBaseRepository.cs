3375749ca37563c9cd0bae34c4c47238e852d656
M_Masoud <mahmoudsaied7760@gmail.com>
2022-09-02 04:10:07 +0200

    Adding EntityBaseRepository

---
+++ Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Booking_Hotel.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<List<T>> GetAllAsync();
+        //include (navigator)
+        Task<List<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetByIdAsync(int id);
+        Task InsertAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+
+    }
+}
