55695de47ed72a74bde20cc02aa85988aed11294
M_Masoud <mahmoudsaied7760@gmail.com>
2022-09-02 08:30:55 +0200

    Adding Helper

---
+++ Data/Helpers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Booking_Hotel.Models;
+using BookingHotel.DTO;
+
+namespace BookingHotel.Helpers
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Branch, BranchDto>();
+        }
+    }
+}
