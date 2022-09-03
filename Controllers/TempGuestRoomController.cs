8a74c5131524db5be37f46ded3c40c8fc18a54b4
M_Masoud <mahmoudsaied7760@gmail.com>
2022-09-03 18:54:05 +0200

    adding TempGuestRoomController

---
+++ Controllers/TempGuestRoomController.cs
@@ -0,0 +1,17 @@
+﻿using Booking_Hotel.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking_Hotel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TempGuestRoomController : ControllerBase
+    {
+        private readonly ITempGuestRoomsService service;
+        public TempGuestRoomController(ITempGuestRoomsService _service)
+        {
+            this.service = _service;
+        }
+
+    }
+}
